e0b287359a80e22bed0bb2a2c95b678600f79f68
Jerry Jäppinen <eiskis@gmail.com>
2020-06-13 13:09:13 +0300

    Fixed promise tests

--- spec/tryPromises.spec.js
+++ spec/tryPromises.spec.js
@@ -11,19 +11,19 @@ describe('tryPromises', () => {
     const seconds = [1000, 2000]
 
     const startTime = new Date()
-    await tryPromises(seconds.map(wait))
+    await tryPromises(...seconds.map(wait))
     const endTime = new Date()
 
     expect(Math.round((endTime - startTime) / 1000)).toEqual(Math.max(...seconds) / 1000)
   })
 
   it('should go through with no error', async (done) => {
-    await tryPromises([failingCallback()])
+    await tryPromises(failingCallback())
     done()
   })
 
   it('should return undefined upon error', async () => {
-    const results = await tryPromises([failingCallback()])
+    const results = await tryPromises(failingCallback())
     expect(results[0]).toBeUndefined()
   })
 })
