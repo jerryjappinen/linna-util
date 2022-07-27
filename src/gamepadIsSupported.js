eedecf58198dea54bb6385336d8ae0a0cfc85396
Jerry Jäppinen <eiskis@gmail.com>
2022-07-27 21:28:35 +0200

    Export

--- src/gamepadIsSupported.js
+++ src/gamepadIsSupported.js
@@ -1,4 +1,4 @@
-export default function () {
+export default () => {
   if (window && window.navigator && window.navigator.getGamepads) {
     return true
   }
--- src/getGamepads.js
+++ src/getGamepads.js
@@ -2,7 +2,7 @@ import gamepadIsSupported from './gamepadIsSupported'
 
 // NOTE: getGamepads returns a GamepadList, not an array
 // https://developer.mozilla.org/en-US/docs/Web/API/Gamepad_API/Using_the_Gamepad_API
-export default function () {
+export default () => {
   if (gamepadIsSupported()) {
     return navigator.getGamepads()
   }
