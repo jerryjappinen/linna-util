84f146963c0e3f14175fa66509c066a994e4dca7
Jerry Jäppinen <eiskis@gmail.com>
2022-08-02 13:55:46 +0200

    Module only

--- contentful.js
+++
@@ -1,8 +0,0 @@
-export { default as assetIsImage } from './src/contentful/assetIsImage'
-export { default as assetIsPdf } from './src/contentful/assetIsPdf'
-export { default as assetIsVideo } from './src/contentful/assetIsVideo'
-export { default as getAssetMimeType } from './src/contentful/getAssetMimeType'
-export { default as getAssetTitle } from './src/contentful/getAssetTitle'
-export { default as getAssetType } from './src/contentful/getAssetType'
-export { default as getAssetUrl } from './src/contentful/getAssetUrl'
-export { default as getImageUrl } from './src/contentful/getImageUrl'
